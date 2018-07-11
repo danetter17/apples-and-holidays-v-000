9571ccf331468bfe6d3e4c90e99995087fa6f044
Daniel Etter <etter.dt@gmail.com>
2018-07-11 21:56:58 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   
   holiday_hash.each do |season, holiday|
-    if holiday.value.include?
+    if holiday.values.include?
   end
   
 end
